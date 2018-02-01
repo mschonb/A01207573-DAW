da77e53fe0b04739257073ef7adbc8e2d1dae23e
Marcelo Schonbrunn <mschonb@gmail.com>
2018-01-31 22:33:07 -0800

    resolver ejercicios lab 6

---
+++ Labs/js/eventos.js
@@ -0,0 +1,19 @@
+function mouseOver(){
+    document.getElementById("mouseO").innerHTML = "<strong> Cambia el estilo de las letras de alguna parte del documento con eventos diferentes a onClic.</strong><br> Pasa el mouse sobre cualquier parte de este texto o sobre la pregunta</p>";
+}
+
+
+function mouseOff(){
+    document.getElementById("mouseO").innerHTML = "<strong>Pasa el mouse sobre mi</strong>";
+}
+
+
+function ayudaMatricula(){
+    document.getElementById("question").innerHTML = "ingresa tu matricula del tec";
+}
+
+var tiempo;
+document.onmousemove = function(){
+  clearTimeout(tiempo);
+  tiempo = setTimeout(function(){alert("Haz algo hombre :/");}, 10000);
+}
--- Labs/js/negativeNumbers.js
+++ Labs/js/negativeNumbers.js
@@ -126,3 +126,5 @@ function inverse(){
     alert(""+aux);
     
 }
+
+
