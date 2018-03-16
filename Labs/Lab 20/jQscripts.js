90885a041f9105037f3941dce871079f8a010dec
mschonb <mschonb@gmail.com>
2018-03-15 23:32:20 -0800

    Agregar Lab 20, todavia falta

---
+++ Labs/Lab 20/jQscripts.js
@@ -0,0 +1,56 @@
+var x = [];
+x = [0,0,0,0,0,0,0,0,0,0];
+var MAX_INT = 9;
+var currentIndex = 9;
+var fullNumbers = 0;
+
+$(document).ready(function(){
+    $("#for").click(function(){
+        addNum();
+    });
+
+    $("#restart").click(function(){
+        restart();
+    });
+});
+
+
+document.getElementById("num").innerHTML = printArray(x);
+
+function addNum(){
+    var i = 0;
+    if(x[currentIndex] < MAX_INT){
+        x[currentIndex]++;
+    }
+    if(x[currentIndex] >= MAX_INT){
+        while(x[currentIndex] >= MAX_INT){
+            currentIndex--;
+            i++;
+            console.log(currentIndex);
+        }
+        x[currentIndex]++;
+
+        while (currentIndex < 9){
+            currentIndex++;
+            x[currentIndex] = 0;
+        }
+    }
+    document.getElementById("num").innerHTML = printArray(x);
+}
+
+function restart(){
+    x = [0,0,0,0,0,0,0,0,0,0];
+    document.getElementById("num").innerHTML = printArray(x);
+}
+
+function printArray(array) {
+    var string = "";
+    for (var i = 0; array.length > i; i++) {
+        string += array[i];
+        if(i == 2 || i == 5){
+            string+= "-"
+        }
+    }
+    return string;
+
+}
